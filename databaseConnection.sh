358222a596b56ddac09e112f844648f52cb99101
Uttkarsh Goel <uttkarshgoel@gmail.com>
2017-02-10 01:10:39 +0530

    Delete databaseConnection.sh

--- databaseConnection.sh
+++
@@ -1,7 +0,0 @@
-if [ $1 = "live" ]; then
-    mysql -hdb-addn.jugnoo.in -u uttkarsh -pw7EyDV8tJbejTCY7;
-elif [ $1 = "test" ]; then
-    mysql -htest.jugnoo.in -u uttkarsh -pVNtKmGBVyqp6J3rS;
-elif [ $1 = "test_neeraj" ]; then
-    mysql -htest.jugnoo.in -u neeraj -pWuXKw7BWS978nVQq;
-fi
